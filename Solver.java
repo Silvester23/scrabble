796ce079adbc1e8e0e7a457fb93feaee294b97d4
Silvester23 <jan@example.com>
2012-06-12 19:16:20 +0200

    update

--- Solver.java
+++ Solver.java
@@ -16,30 +16,5 @@ public class Solver {
 	
 	}
 	
-	
-	
-	
-	
-/*	
-public static BloomFilter<String> createBloomFilter() {
-		
-		double falsePositiveProbability = 0.1;
-		int expectedSize = 327314;
-		BloomFilter<String> bloomFilter = new BloomFilter<String>(falsePositiveProbability, expectedSize);
-		
-		try {
-			FileInputStream fis = new FileInputStream("C:/Users/Jan/Documents/projekte/Java/Scrabble Solver/res/dict.txt");
-			DataInputStream in = new DataInputStream(fis);
-			BufferedReader br = new BufferedReader(new InputStreamReader(in));
-			String line;
-			while((line = br.readLine()) != null) {
-				bloomFilter.add(line);
-			}
-		} catch (Exception e){
-			System.out.println("Exception: " + e.getMessage());
-		}
-		return bloomFilter;
-	}
-*/	
 
 }
